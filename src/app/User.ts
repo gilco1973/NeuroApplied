2af3dce7dcaa9d6e78ef888d3ce444ff43efcf01
Gil Klainert <gil.klainert@gmail.com>
2019-06-01 23:08:04 +0300

    css fix

--- src/app/User.ts
+++ src/app/User.ts
@@ -1,14 +1,13 @@
-export class User
-{
-  id: any;
-  name: any;
-  email: any;
-  imageUrl: any;
-  givenName: any;
-  familyName: any;
-  token: any;
-  save() {
-    console.log(this);
-  }
-    constructor(){}
+export class User {
+    id: any;
+    name: any;
+    email: any;
+    imageUrl: any;
+    givenName: any;
+    familyName: any;
+    token: any;
+    save() {
+        console.log(this);
+    }
+    constructor() { }
 }

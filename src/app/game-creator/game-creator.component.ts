3b69a594f3abc43e5adf0269c03ebb5ed7db2c45
Gil Klainert <gil.klainert@gmail.com>
2019-06-03 09:36:34 +0300

    use scss instead of css

--- src/app/game-creator/game-creator.component.ts
+++ src/app/game-creator/game-creator.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-game-creator',
   templateUrl: './game-creator.component.html',
-  styleUrls: ['./game-creator.component.css']
+  styleUrls: ['./game-creator.component.scss']
 })
 export class GameCreatorComponent implements OnInit {
 

3561c3784e1a6342bd72c714328ab76c6976ec25
Gil Klainert <gil.klainert@gmail.com>
2019-06-04 21:55:01 +0300

    commented out client side for sign in with google, using server side

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -45,25 +45,25 @@ export class LoginComponent implements OnInit, AfterViewInit {
 
 
 
-  signInWithGoogle() {
-    gapi.load('auth2', () => {
-      gapi.auth2.init();
-      const googleAuth = gapi.auth2.getAuthInstance();
-      googleAuth.then(() => {
-        googleAuth.signIn({ scope: 'profile email' }).then(googleUser => {
-          this.code = googleUser.getAuthResponse().id_token;
-          this.loginWithSocial('google');
-          /* const profile = googleUser.getBasicProfile();
-          console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-          console.log('ID Token: ' + id_token);
-          console.log('Name: ' + profile.getName());
-          console.log('Image URL: ' + profile.getImageUrl());
-          console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present. */
+  // signInWithGoogle() {
+  //   gapi.load('auth2', () => {
+  //     gapi.auth2.init();
+  //     const googleAuth = gapi.auth2.getAuthInstance();
+  //     googleAuth.then(() => {
+  //       googleAuth.signIn({ scope: 'profile email' }).then(googleUser => {
+  //         this.code = googleUser.getAuthResponse().id_token;
+  //         this.loginWithSocial('google');
+  //         /* const profile = googleUser.getBasicProfile();
+  //         console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  //         console.log('ID Token: ' + id_token);
+  //         console.log('Name: ' + profile.getName());
+  //         console.log('Image URL: ' + profile.getImageUrl());
+  //         console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present. */
 
-        });
+  //       });
 
-      });
-    });
-  }
+  //     });
+  //   });
+  // }
 }
 
